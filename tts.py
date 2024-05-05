708fe154b36648be6ea5b34d25c85cb357b8d734
adrian <adrian>
2024-05-05 09:03:04 +0200

    tts script refactoring

--- tts.py
+++ tts.py
@@ -2,34 +2,40 @@ from itertools import islice
 
 from TTS.api import TTS
 
-def split_bulk_file(file_path):
-    nr_of_lines = 100
-    files = []
-
-    with open(file_path, encoding='utf8') as T:
-        for i, lines in enumerate(iter(lambda: list(islice(T, nr_of_lines)), []), 1):
-
-            out_file = file_path.replace(".txt", str(i) + ".txt")
-            files.append(out_file)
-
-            with open(out_file, "w", encoding='utf8') as f:
-                f.writelines(lines)
-
-    return files
-
-def from_file(file_path):
-    with open(file_path, 'r', encoding='utf8') as file:
-        return file.read().replace('\n', ' ')
-
+polish_model = "tts_models/pl/mai_female/vits"
+utf_8 = 'utf8'
+padding = '-{:03}'
+lines_per_file = 100
 
 def read_to_file(file_path):
-    tts = TTS("tts_models/pl/mai_female/vits")
+    tts = TTS(polish_model)
 
-    file_paths = split_bulk_file(file_path)
+    file_paths = split_into_multiple_files(file_path)
 
-    for file_path in file_paths:
-        content = from_file(file_path)
+    for path in file_paths:
+        content = file_content_to_string(path)
 
-        tts.tts_to_file(text=content, file_path=file_path + '.wav')
+        tts.tts_to_file(text=content, file_path=path + '.wav')
+
+
+def split_into_multiple_files(input_file_path):
+    out_paths = []
+
+    with open(input_file_path, encoding=utf_8) as file:
+        for i, lines in enumerate(iter(lambda: list(islice(file, lines_per_file)), []), 1):
+
+            ith_out_path = input_file_path + padding.format(i)
+            out_paths.append(ith_out_path)
+
+            with open(ith_out_path, "w", encoding=utf_8) as f:
+                f.writelines(lines)
+
+    return out_paths
+
+
+def file_content_to_string(file_path):
+    with open(file_path, 'r', encoding=utf_8) as file:
+        return file.read().replace('\n', ' ')
+
 
 read_to_file('resources/tts_input.txt')
