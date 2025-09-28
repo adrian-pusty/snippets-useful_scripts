b9375c676f37ecd9c050a031bf7ff27fc60bafdb
Adrian Pusty <51136868+adrian-pusty@users.noreply.github.com>
2025-09-28 14:35:48 +0200

    Create merge_two_git_repositories.sh

---
+++ merge_two_git_repositories.sh
@@ -0,0 +1,12 @@
+target_project_path=/path/to/target/project #todo
+source_project_path=/path/to/source/project #todo
+source_project_branch=master #todo
+
+cd "$source_project_path"
+git checkout "$source_project_branch"
+
+cd "$target_project_path"
+git remote add source-project "$source_project_path"
+git fetch source-project --tags
+git merge --allow-unrelated-histories source-project/"$source_project_branch"
+git remote remove source-project
